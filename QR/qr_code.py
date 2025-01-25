36cf60397e3a3b3a85dd692cfef6416dfd63065f
Debjani Dutta Gupta <158575148+el23ddg@users.noreply.github.com>
2025-01-25 23:43:58 +0530

    Update qr_code.py

--- QR/qr_code.py
+++ QR/qr_code.py
@@ -1,8 +1,8 @@
 # Generate QR code with Python
 import qrcode
 website_link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
+qr = qrcode.QRCode(version = 1, box_size = 5, border = 5) #adds the details of the box
 qr.add_data(website_link)
 qr.make()
-img = qr.make_image(fill_color = 'black', back_color = 'white')
+img = qr.make_image(fill_color = 'black', back_color = 'white') #adds the colour of the rr code
 img.save('youtube_qr.png')
